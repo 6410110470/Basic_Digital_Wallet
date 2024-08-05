b92baec984a6aa114d4a9a6d2a0130be5410ec96
6410110470 <6410110470@email.psu.ac.th>
2024-08-05 21:42:06 +0700

    add wallet

--- digimon/routers/__init__.py
+++ digimon/routers/__init__.py
@@ -1,8 +1,9 @@
 from . import items
 from . import merchants
-
+from . import wallets
 
 
 def init_routers(app):
     app.include_router(items.router)
     app.include_router(merchants.router)
+    app.include_router(wallets.router)
